7be280487b9ab365bcedc39cc75ff1da4ee29e67
ram-rohith <ramrohith12@>
2022-05-09 10:27:29 -0400

    new

--- module-4/script.js
+++ module-4/script.js
@@ -1,8 +1,8 @@
 (function (window) {
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-	for (var i=0; i < names.length; i++ /* fill in parts of the 'for' loop to loop over names array */) {
+	for (var i=0; i < names.length; i++) {
 	  var firstLetter = names[i].charAt(0).toLowerCase();
-		if (firstLetter == "j" /* fill in condition here */) {
+		if (firstLetter == "j") {
 		  byeSpeaker.speak(names[i]);
 		} else {
 		  helloSpeaker.speak(names[i]);
